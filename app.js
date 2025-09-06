73994893e375f8b26ea5eed2551462f760721b60
anirudhchakraborty <preetiujjal@gmail.com>
2025-09-07 05:15:45 +0530

    Loading animation added

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.body.style.backgroundColor = "black";
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.classList.add('fade-in-bg');
+  var loginBox = document.querySelector('.login-container');
+  if (loginBox) {
+    loginBox.classList.add('fade-in');
+  }
+});
