be723445a2592740b525a6f72273b9eefa1558d3
anirudhchakraborty <preetiujjal@gmail.com>
2025-09-07 21:55:55 +0530

    Sing Up Completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.body.classList.add('fade-in-bg');
-  var loginBox = document.querySelector('.login-container');
+  var loginBox = document.querySelector('.login-container,.signup-container');
   if (loginBox) {
     loginBox.classList.add('fade-in');
   }
